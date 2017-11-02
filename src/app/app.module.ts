bb9b3276775fab25e168374f5e31e0044b991a51
abubakarzafar77 <abubakar.zafar@purelogics.net>
2017-11-02 13:35:13 -0700

    create all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './componnets/dashboard/dashboard.component';
+import { ClientsComponent } from './componnets/clients/clients.component';
+import { ClientDetailsComponent } from './componnets/client-details/client-details.component';
+import { AddClientComponent } from './componnets/add-client/add-client.component';
+import { EditClientComponent } from './componnets/edit-client/edit-client.component';
+import { NavbarComponent } from './componnets/navbar/navbar.component';
+import { SidebarComponent } from './componnets/sidebar/sidebar.component';
+import { PageNotFoundComponent } from './componnets/page-not-found/page-not-found.component';
+import { LoginComponent } from './componnets/login/login.component';
+import { RegisterComponent } from './componnets/register/register.component';
+import { SettingsComponent } from './componnets/settings/settings.component';
 
+
+const appRoutes: Routes = [
+    { path: '', component: DashboardComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+
+]
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        ClientsComponent,
+        ClientDetailsComponent,
+        AddClientComponent,
+        EditClientComponent,
+        NavbarComponent,
+        SidebarComponent,
+        PageNotFoundComponent,
+        LoginComponent,
+        RegisterComponent,
+        SettingsComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
